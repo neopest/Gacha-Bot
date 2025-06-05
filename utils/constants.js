b55da5d3b8da422c9b9052a5a7add41a6e467386
neopest <82114464+neopest@users.noreply.github.com>
2025-06-04 23:24:17 -0400

    Create constants.js

---
+++ utils/constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+    RARITY_ORDER: ["Common", "Uncommon", "Rare", "Legendary"],
+    RARITY_PROBABILITIES: {
+        common: 0.45,
+        uncommon: 0.3,
+        rare: 0.18,
+        legendary: 0.07
+    },
+    SUPER_SPIN_PROBABILITIES: {
+        rare: 0.7,
+        legendary: 0.3
+    },
+    PERMISSIONS: {
+        ADMINISTRATOR: 'Administrator'
+    }
+};
